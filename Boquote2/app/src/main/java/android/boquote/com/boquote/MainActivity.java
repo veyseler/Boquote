60ce110503ba9260a51814b440ff998007b955d3
Veysel Er <veyseler@sabanciuniv.edu>
2017-03-15 20:55:43 +0300

    Initial commit

---
+++ Boquote2/app/src/main/java/android/boquote/com/boquote/MainActivity.java
@@ -0,0 +1,13 @@
+package android.boquote.com.boquote;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
